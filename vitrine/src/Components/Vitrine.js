5a6c0e80c72179fc55d9e0c822c592f38925252c
rubencavalcanti <rubencavalcanti@gmail.com>
2022-07-29 12:51:53 -0300

    criação do componentes, arquivos css e configuração das rotas

---
+++ vitrine/src/Components/Vitrine.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Vitrine = () => {
+  return (
+    <div>
+        <h1>Vitrine</h1>
+        <NavLink to='/notebook'>Produto</NavLink>
+    </div>
+  )
+}
+
+export default Vitrine
